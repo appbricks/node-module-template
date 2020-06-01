dc33f922fc6d0f94a5dab682eb9a1c0320796eea
Mevan Samaratunga <msamaratunga@Mevans-MacBook-Pro.local>
2020-06-01 10:00:27 -0400

    set tab width to 2

--- src/__tests__/hello-npm.test.ts
+++ src/__tests__/hello-npm.test.ts
@@ -1,5 +1,5 @@
 import { HelloNPM } from '../index';
 
 test('Hello NPM Test', () => {
-    expect(HelloNPM('AppBricks')).toBe('Hello AppBricks to the NPM world!');
+  expect(HelloNPM('AppBricks')).toBe('Hello AppBricks to the NPM world!');
 });
